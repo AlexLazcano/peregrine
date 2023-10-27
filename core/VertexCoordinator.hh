4671f8e9d47f468c0eb7fe08585d6b967b07cdee
Alexis Lazcano <alazcano@sfu.ca>
2023-10-26 22:13:42 -0700

    Testing vertex coord

--- core/VertexCoordinator.hh
+++ core/VertexCoordinator.hh
@@ -9,6 +9,7 @@ namespace Peregrine
         MPI_Status status;
         int count;
         uint64_t buffer[2];
+        // Tag 0 - Sending empty message to 0
         MPI_Send(&buffer, 0, MPI_UINT64_T, 0, 0, MPI_COMM_WORLD);
 
         MPI_Probe(0, 1, MPI_COMM_WORLD, &status);
@@ -29,9 +30,9 @@ namespace Peregrine
     class VertexQueue
     {
     private:
-        uint64_t curr = 0;
+        std::atomic<uint64_t> curr = 0;
         uint64_t end;
-        int32_t step = 11;
+        int32_t step = 10;
         bool finished = false;
         int number_of_consumers;
 
@@ -43,25 +44,33 @@ namespace Peregrine
         }
         bool get_v_range(uint64_t *start_range, uint64_t *end_range)
         {
-            if (this->curr >= this->end)
+            while (true)
             {
-                *start_range = -1, *end_range = -1;
-                return false;
+                if (this->curr >= this->end)
+                {
+                    *start_range = -1, *end_range = -1;
+                    return false;
+                }
+                uint64_t before = this->curr;
+                
+                *start_range = before;
+                if (before + this->step < this->end)
+                {
+                    *end_range = before + this->step;
+                }
+                else
+                {
+                    *end_range = this->end;
+                }
+
+                bool successful = this->curr.compare_exchange_strong(before, *end_range);
+                if (successful)
+                {
+                   break;
+                }
+                
             }
 
-            uint64_t start = this->curr;
-
-            *start_range = start;
-            if (start + this->step < this->end)
-            {
-                *end_range = start + this->step;
-            }
-            else
-            {
-                *end_range = this->end;
-            }
-
-            this->curr = *end_range;
             return true;
         }
         void coordinate()
@@ -71,16 +80,18 @@ namespace Peregrine
             uint64_t buffer[2];
             while (!this->finished && processesFinished < this->number_of_consumers)
             {
+                // Tag 0 - Receive empty message and see who its from
                 MPI_Recv(&buffer, 0, MPI_UINT64_T, MPI_ANY_SOURCE, 0, MPI_COMM_WORLD, &status);
                 bool successful = get_v_range(&buffer[0], &buffer[1]);
-                // printf("start %ld, end %ld \n", buffer[0], buffer[1]);
                 if (!successful)
                 {
+                    // Tag 1 - Ran out of messages to send
                     MPI_Send(&buffer, 0, MPI_UINT64_T, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
 
                     processesFinished++;
                     continue;
                 }
+                // Tag 1 - Send back range since it was successful
                 MPI_Send(&buffer, 2, MPI_UINT64_T, status.MPI_SOURCE, 1, MPI_COMM_WORLD);
             }
         }
