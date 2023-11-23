90d9b5bc393695a6b48bcd50f13cbdfcd0162558
Alexis Lazcano <alazcano@sfu.ca>
2023-11-22 17:44:06 -0800

    testing files slurm

--- slurm/countDist-8cpu.sh
+++ slurm/countDist-8cpu.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #
-#SBATCH --cpus-per-task=1
+#SBATCH --cpus-per-task=8
 #SBATCH --time=03:30
 #SBATCH --mem=5G
 #SBATCH --partition=slow
 #SBATCH --ntasks=1
 
 
-srun /home/$USER/master/bin/count ../data/citeseer 5-motifs 1
+srun /home/$USER/peregrine/bin/countMPI ../data/citeseer 6-motifs 8
--- slurm/countQuery.sh
+++ slurm/countQuery.sh
@@ -4,7 +4,7 @@
 #SBATCH --time=03:30
 #SBATCH --mem=10G
 #SBATCH --partition=slow
-#SBATCH --ntasks=1
+#SBATCH --ntasks=4
 
 
-srun /home/$USER/peregrine/bin/countMPI ../data/citeseer 5-motifs 1
+srun /home/$USER/peregrine/bin/countMPI ../data/citeseer ../query/test.graph 1
